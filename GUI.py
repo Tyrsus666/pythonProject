f522184f62576d0ec2cc1dad5130c61d6cd8c525
Tyrsus <tyrsus666@gmail.com>
2025-01-25 01:39:50 +0100

    bugfix

--- GUI.py
+++ GUI.py
@@ -18,6 +18,7 @@ def read_config():
 
 def update_config(*args):
     config = configparser.ConfigParser()
+    config.add_section('General')
     config.set('General', 'kingdom', kingdom.get())
     config.set('General', 'scans', governor.get())
     with open('config.ini', 'w') as config_file:
@@ -35,6 +36,7 @@ def showGov(*args):
 def callback(*args):
     showKD()
     showGov()
+    update_config()
 
 
 ##### GUI #####
@@ -42,37 +44,39 @@ def callback(*args):
 # window
 mainWin = Tk()
 mainWin.title('SKDAS')
-mainWin.geometry('500x200')
+mainWin.geometry('500x250')
 
 # variables
 kingdom = StringVar()
-kingdom.set(config_data['kingdom']) # devault kingdom
+kingdom.set(config_data['kingdom'])  # default kingdom
 Governor = StringVar()
 governor = StringVar()
-governor.set(config_data['governor']) # devault value
+governor.set(config_data['governor'])  # default value
 Kingdom = StringVar()
 
 # left frame
-leftFrame = ttk.Frame(mainWin, borderwidth=20, relief='flat', width=200, height=150, padding='10 10 10 10')
+leftFrame = ttk.Frame(mainWin, borderwidth=20, relief='flat', width=200, height=200, padding='10 10 10 10')
 leftFrame.grid_propagate(0)
 leftFrame.grid(row=0, column=0)
 
 # left content
 label1 = ttk.Label(leftFrame, text='Select your Kingdom:')
-label1.grid(column=0, row=0)
+label1.grid(column=0, row=0, columnspan=2)
 entry_KD = ttk.Entry(leftFrame, textvariable=kingdom, width=4)
-entry_KD.grid(column=0, row=1)
+entry_KD.grid(column=0, row=1, columnspan=2)
 entry_KD.focus()
 label2 = ttk.Label(leftFrame, text='Governors to scan:')
-label2.grid(column=0, row=2)
+label2.grid(column=0, row=2, columnspan=2)
 entry_Gov = ttk.Entry(leftFrame, textvariable=governor, width=4)
-entry_Gov.grid(column=0, row=3)
+entry_Gov.grid(column=0, row=3, columnspan=2)
 
-button1 = ttk.Button(leftFrame, text='Scan', command=lambda: [showKD(), showGov(), update_config()])
-button1.grid(column=0, row=4)
+button1 = ttk.Button(leftFrame, text='Scan', command=lambda: [callback()])
+button1.grid(column=0, row=4, pady=5)
+button2 = ttk.Button(leftFrame, text='Cancel', command=lambda: [mainWin.destroy()])
+button2.grid(column=1, row=4, pady=5)
 
 # right frame
-rightFrame = ttk.Frame(mainWin, borderwidth=5, relief='sunken', width=300, height=150)
+rightFrame = ttk.Frame(mainWin, borderwidth=5, relief='sunken', width=300, height=200)
 rightFrame.grid_propagate(0)
 rightFrame.grid(row=0, column=1)
 
@@ -80,11 +84,11 @@ rightFrame.grid(row=0, column=1)
 label3 = ttk.Label(rightFrame, text='Name:')
 label3.grid(column=0, row=0, sticky=W, ipadx=5)
 label4 = ttk.Label(rightFrame, textvariable=Kingdom)
-label4.grid(column=1, row=0)
+label4.grid(column=1, row=0, sticky=W)
 label5 = ttk.Label(rightFrame, text='ID:')
 label5.grid(column=0, row=1, sticky=W)
 label6 = ttk.Label(rightFrame, textvariable=Governor)
-label6.grid(column=1, row=1)
+label6.grid(column=1, row=1, sticky=W)
 
 
 # progressbar
