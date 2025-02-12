84c0adf3bb88279c1c43eede9fb6d5261a9dc5d4
Tyrsus <tyrsus666@gmail.com>
2025-02-12 10:52:08 +0100

    Version1.1

--- GUI.py
+++ GUI.py
@@ -5,16 +5,20 @@ from tkinter import *
 from tkinter import ttk
 import configparser
 import time
+import tkinter as tk
+from tkinter import messagebox
+import requests
 
 
 def read_config():
     config = configparser.ConfigParser()
     config.read('config.ini')
-    kingdom = config.get('General','kingdom')
-    governor = config.get('General','scans')
+    kingdom = config.get('General', 'kingdom')
+    governor = config.get('General', 'scans')
 
     config_values = {'kingdom': kingdom, 'governor': governor}
     return config_values
+
 
 def update_config(*args):
     config = configparser.ConfigParser()
@@ -24,19 +28,45 @@ def update_config(*args):
     with open('config.ini', 'w') as config_file:
         config.write(config_file)
 
+
 if __name__ == "__main__":
     config_data = read_config()
+
 
 def showKD(*args):
     Kingdom.set(kingdom.get())
 
+
 def showGov(*args):
     Governor.set(governor.get())
+
 
 def callback(*args):
     showKD()
     showGov()
     update_config()
+
+
+def check_for_updates():
+    response = requests.get("https://api.github.com/repos/nikolakis1919/RokTracker/releases/latest")
+    if (response.json()["name"]) != version:
+        bo = tk.Tk()
+        bo.withdraw()
+        messagebox.showinfo("Tool is outdated",
+                            "New version is available on github repository. It is highly recommended to update the tool!")
+        bo.destroy()
+
+
+def update_tool():
+    # window
+    updateWin = Tk()
+    updateWin.title('Update')
+    updateWin.geometry('200x200')
+
+    # content
+    label = ttk.Label(updateWin, text='Update available.')
+    label.grid(row=0, column=0)
+    updateWin.mainloop()
 
 
 ##### GUI #####
@@ -53,6 +83,7 @@ Governor = StringVar()
 governor = StringVar()
 governor.set(config_data['governor'])  # default value
 Kingdom = StringVar()
+version = 1.1
 
 # left frame
 leftFrame = ttk.Frame(mainWin, borderwidth=20, relief='flat', width=200, height=200, padding='10 10 10 10')
@@ -83,50 +114,49 @@ rightFrame.grid(row=0, column=1)
 # right content
 label3 = ttk.Label(rightFrame, text='Rank:')
 label3.grid(column=0, row=0, sticky=W, ipadx=5)
-#label4 = ttk.Label(rightFrame, textvariable=i+1)
-#label4.grid(column=1, row=0, sticky=W)
+# label4 = ttk.Label(rightFrame, textvariable=i+1)
+# label4.grid(column=1, row=0, sticky=W)
 label5 = ttk.Label(rightFrame, text='Name:')
 label5.grid(column=0, row=1, sticky=W)
-#label6 = ttk.Label(rightFrame, textvariable=gov_name)
-#label6.grid(column=1, row=1, sticky=W)
+# label6 = ttk.Label(rightFrame, textvariable=gov_name)
+# label6.grid(column=1, row=1, sticky=W)
 label7 = ttk.Label(rightFrame, text='ID:')
 label7.grid(column=0, row=2, sticky=W)
-#label8 = ttk.Label(rightFrame, textvariable=tointcheck(gov_id))
-#label8.grid(column=1, row=2, sticky=W)
+# label8 = ttk.Label(rightFrame, textvariable=tointcheck(gov_id))
+# label8.grid(column=1, row=2, sticky=W)
 label9 = ttk.Label(rightFrame, text='Power:')
 label9.grid(column=0, row=3, sticky=W)
-#label10 = ttk.Label(rightFrame, textvariable=tointcheck(gov_power))
-#label10.grid(column=1, row=3, sticky=W)
+# label10 = ttk.Label(rightFrame, textvariable=tointcheck(gov_power))
+# label10.grid(column=1, row=3, sticky=W)
 label11 = ttk.Label(rightFrame, text='Kill Points:')
 label11.grid(column=0, row=4, sticky=W)
-#label12 = ttk.Label(rightFrame, textvariable=tointcheck(gov_killpoints))
-#label12.grid(column=1, row=4, sticky=W)
+# label12 = ttk.Label(rightFrame, textvariable=tointcheck(gov_killpoints))
+# label12.grid(column=1, row=4, sticky=W)
 label13 = ttk.Label(rightFrame, text='Deads:')
 label13.grid(column=0, row=5, sticky=W)
-#label14 = ttk.Label(rightFrame, textvariable=tointcheck(gov_dead))
-#label14.grid(column=1, row=5, sticky=W)
+# label14 = ttk.Label(rightFrame, textvariable=tointcheck(gov_dead))
+# label14.grid(column=1, row=5, sticky=W)
 label15 = ttk.Label(rightFrame, text='T4 Kills:')
 label15.grid(column=0, row=6, sticky=W)
-#label16 = ttk.Label(rightFrame, textvariable=tointcheck(gov_kills_tier4))
-#label16.grid(column=1, row=6, sticky=W)
+# label16 = ttk.Label(rightFrame, textvariable=tointcheck(gov_kills_tier4))
+# label16.grid(column=1, row=6, sticky=W)
 label17 = ttk.Label(rightFrame, text='T5 Kills:')
 label17.grid(column=0, row=7, sticky=W)
-#label18 = ttk.Label(rightFrame, textvariable=tointcheck(gov_kills_tier5))
-#label18.grid(column=1, row=7, sticky=W)
+# label18 = ttk.Label(rightFrame, textvariable=tointcheck(gov_kills_tier5))
+# label18.grid(column=1, row=7, sticky=W)
 label19 = ttk.Label(rightFrame, text='Rss Assistance:')
 label19.grid(column=0, row=8, sticky=W)
-#label20 = ttk.Label(rightFrame, textvariable=tointcheck(gov_rss_assistance))
-#label20.grid(column=1, row=8, sticky=W)
+# label20 = ttk.Label(rightFrame, textvariable=tointcheck(gov_rss_assistance))
+# label20.grid(column=1, row=8, sticky=W)
 label21 = ttk.Label(rightFrame, text='Helptimes:')
 label21.grid(column=0, row=9, sticky=W)
-#label22 = ttk.Label(rightFrame, textvariable=tointcheck(gov_helptimes))
-#label22.grid(column=1, row=9, sticky=W)
+# label22 = ttk.Label(rightFrame, textvariable=tointcheck(gov_helptimes))
+# label22.grid(column=1, row=9, sticky=W)
 
 
 # progressbar
 progress = ttk.Progressbar(mainWin, orient=HORIZONTAL, length=450, mode='determinate')
 progress.grid(column=0, row=1, columnspan=2, pady=10)
-
 
 # using keyboard return
 mainWin.bind('<Return>', callback)
